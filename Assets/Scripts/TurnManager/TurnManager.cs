05dc5914bef72d6bf0d9f424b709e8ac673f2d01
Ronja <ronja_mattsson@outlook.com>
2023-11-21 14:46:30 +0100

    ending the turn replenishes move points

--- Assets/Scripts/TurnManager/TurnManager.cs
+++ Assets/Scripts/TurnManager/TurnManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,8 @@ public class TurnManager : MonoBehaviour // classen blir en singleton
 {
     public static TurnManager Instance;
     public bool isPlayerTurn = true;
+
+    [SerializeField] TMP_Text tempTurnText;
 
     public void Awake()
     {
@@ -17,17 +20,39 @@ public class TurnManager : MonoBehaviour // classen blir en singleton
             Destroy(this.gameObject);
         #endregion
 
+        UpdateUI();
+
     }
     public void EndTurn() //when clicked on End Turn Button
     {
         if (isPlayerTurn)
         {
             isPlayerTurn = false;
+            MovementManager.Instance.takingMoveAction = false;
+
+            UpdateUI();
             StartCoroutine(AITurn());//Ändra sen
 
             //TODO disable player interaction
         }
         
+    }
+
+    public void GoToPlayerTurn()
+    {
+        if (isPlayerTurn)
+        {
+            Debug.LogError("Something tried to pass to the player, during the players turn");
+            return;
+        }
+
+        isPlayerTurn = true;
+        MovementManager.Instance.takingMoveAction = true; // Change later
+        foreach (Unit u in UnitStorage.Instance.playerUnits)
+        {
+            u.movePointsCur = u.movePointsMax;
+        }
+        UpdateUI();
     }
 
     public System.Collections.IEnumerator AITurn()
@@ -37,6 +62,14 @@ public class TurnManager : MonoBehaviour // classen blir en singleton
         //kalla på scriptet för AI movement 
 
         // Switch back to player's turn
-        isPlayerTurn = true;
+        GoToPlayerTurn();
+    }
+
+    public void UpdateUI()
+    {
+        if (tempTurnText == null) return;
+
+        if (isPlayerTurn) tempTurnText.text = "Player Turn";
+        else tempTurnText.text = "AI Turn";
     }
 }
---
+++ Assets/Scripts/Units/Management/UnitStorage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStorage : MonoBehaviour
+{
+    public static UnitStorage Instance;
+
+    public List<Unit> playerUnits = new List<Unit>();
+    public List<Unit> npcUnits = new List<Unit>();
+
+    private void Awake()
+    {
+        #region Singleton
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+        #endregion
+    }
+}
