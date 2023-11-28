472f99ea90c246a8ee01c9a52d701e3b319971aa
FancyDuckling <FancyDuckling@users.noreply.github.com>
2023-11-28 15:22:11 +0100

    added dynamite card
    duplicated Ronjas scene

--- Assets/Scripts/Card scripts/Behaivour/DynamiteCard.cs
+++ Assets/Scripts/Card scripts/Behaivour/DynamiteCard.cs
@@ -15,7 +15,7 @@ public class DynamiteCard : PlayCard
 
     [SerializeField]
     private int multiplier = 4;
-    private int explosionRange;
+    private int explosionRange = 2;
     
 
     public override void ExecuteBehaivour(Tile onTile, Unit byUnit)
@@ -35,19 +35,42 @@ public class DynamiteCard : PlayCard
 
             Destroy(dynamite);
 
-                    //onTile.occupant.TakeDamage(byUnit.attackRange  ); //2 rutor åt alla håll
+            ApplyDamageRadius(onTile, byUnit);       
 
                     //kolla grannar och lägg till i listan i alla fyra håll, for loop med dig eller damge
                     //den kan INTE hamna på dirt men kan pränga dirt
          }
 
-
                 // Apply damage to units in a 2-tile radius
-            
-
-        
     }
-   
 
 
+    public void ApplyDamageRadius(Tile centerTile, Unit byUnit)
+    {
+        Tile[] allTiles = FindObjectsOfType<Tile>();
+
+        // Loop through all tiles to find neighbors within the explosion radius
+        foreach (Tile otherTile in allTiles)
+        {
+            // Skip the center tile
+            if (otherTile == centerTile)
+                continue;
+
+            // Calculate the distance between tiles
+            int distanceX = Mathf.Abs(centerTile.x - otherTile.x);
+            int distanceY = Mathf.Abs(centerTile.y - otherTile.y);
+
+            // Check if the otherTile is within the explosion radius
+            if (distanceX <= explosionRange && distanceY <= explosionRange)
+            {
+                // Check if there is an occupant on the neighboring tile
+                if (otherTile.occupant != null)
+                {
+                    // Apply damage to the unit on the neighboring tile
+                    otherTile.occupant.TakeDamage(byUnit.power * multiplier);
+                }
+            }
+        }
+
+    }
 }
