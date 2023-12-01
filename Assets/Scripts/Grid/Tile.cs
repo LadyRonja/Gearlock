d675face673b06f905663ef166a2fe9723a0c880
FancyDuckling <FancyDuckling@users.noreply.github.com>
2023-12-01 09:48:27 +0100

    commented out blue tile in tile script

--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -84,12 +84,12 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
     public void OnPointerDown(PointerEventData eventData)
      {
 
-        if (occupant != null && occupant.playerBot)
+       /* if (occupant != null && occupant.playerBot)
         {
             myMR.material.color = Color.blue;
 
             HighlightNeighbours();
-        }
+        }*/
             
 
         // Select My Unit
@@ -146,7 +146,7 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
 
     //TEST ELIN when you click on a tile the neighbors gets blue
 
-    public void OnPointerUp(PointerEventData eventData)
+   /* public void OnPointerUp(PointerEventData eventData)
     {
        
         myMR.material.color = Color.white;
@@ -187,7 +187,7 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
 
         if (neighbourW != null)
             neighbourW.myMR.material.color = Color.white;
-    }
+    }*/
 
     
 }
