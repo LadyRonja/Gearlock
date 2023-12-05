06483273c7947e91ea2b24a1cbc028ad9b72e544
FancyDuckling <FancyDuckling@users.noreply.github.com>
2023-12-05 15:41:59 +0100

    added font

--- Assets/Scripts/Grid/Dirt.cs
+++ Assets/Scripts/Grid/Dirt.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class Dirt : MonoBehaviour
 {
     public MeshRenderer gfx;
+    public void OnMouseEnter()
+    {
+        PlayCard currentCard = ActiveCard.Instance.transform.GetComponentInChildren<PlayCard>();
+
+        if (currentCard != null)
+        {
+            if (currentCard.GetType().Equals(typeof(DigCard)))
+            {
+                MouseControl.instance.Dig();
+                Debug.Log("Changing cursor to Dig");
+            }
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        MouseControl.instance.Default();
+    }
 }
